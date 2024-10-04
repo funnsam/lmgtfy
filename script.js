aec05786ad27044c3cab138fa66f12db2cb2fbab
funnsam <funnsam8@gmail.com>
2024-10-04 15:51:29 +0000

    nice

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const query_str = window.location.search;
-const params = new URLSearchParams(query_str);
+const params = new URLSearchParams(window.location.search);
 const query = params.get("q");
 
 const search_text = document.getElementById("searchtext");
@@ -15,27 +14,19 @@ search_text.ondragenter = edit_search_text;
 search_text.ondragleave = edit_search_text;
 search_text.ondragover = edit_search_text;
 search_text.ondrop = edit_search_text;
-search_text.innerText = "";
 
 if (query !== null) {
-    function select_last_char() {
-        var range = document.createRange();
-        var len = search_text.innerText.length;
-        range.setStart(search_text.firstChild, len);
-        range.setEnd(search_text.firstChild, len);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-    }
+    search_text.innerText = "";
 
-    function type_letter() {
-        search_text.innerText += query.charAt(search_text.innerText.length);
-        select_last_char();
+    function select_last_char() {
+        window.getSelection().setPosition(search_text.firstChild, search_text.innerText.length);
     }
 
     function type_letters() {
         return new Promise((resolve) => setTimeout(() => {
-            type_letter();
+            search_text.innerText += query.charAt(search_text.innerText.length);
+            select_last_char();
+
             if (search_text.innerText.length < query.length) {
                 type_letters().then(resolve);
             } else {
@@ -47,6 +38,7 @@ if (query !== null) {
     step_text.innerText = "Step 1. type your question";
     (new Promise((resolve) => {
         setTimeout(() => {
+            window.getSelection().selectAllChildren(search_text);
             type_letters().then(resolve);
         }, 3000);
     })).then(() => {
@@ -56,6 +48,18 @@ if (query !== null) {
             search_btn.style.animationName = "scale";
             search_btn.style.animationDuration = "1s";
             search_btn.style.animationIterationCount = "infinite";
+
+            search_btn.onclick = () => {
+                window.location = "https://www.google.com/search" + window.location.search;
+                step_text.innerText = "c'mon do i really need to teach you this?";
+            };
         }, 1000);
     });
+} else {
+    search_btn.innerText = "Get URL";
+    search_btn.onclick = () => {
+        var p = new URL(window.location);
+        p.searchParams.set("q", search_text.innerText);
+        step_text.innerText = `Normal search: ${p}\nImage search: ${p}&udm=2`;
+    };
 }
